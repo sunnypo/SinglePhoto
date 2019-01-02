f98da72cbf429d4e8914bf67cd346cbbed919ca0
Sunny Huang <sunny208@me.com>
2019-01-01 21:54:37 -0500

    Initial Commit

---
+++ SinglePhoto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SinglePhoto
+//
+//  Created by 黄思源 on 2019/1/1.
+//  Copyright © 2019 supencil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
